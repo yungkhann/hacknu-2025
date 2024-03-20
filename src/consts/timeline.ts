5e5b8576b5fc144ed610468e4b9ef76d2f54d8ab
Sanzhar <sanxzhar@gmail.com>
2024-03-20 23:30:58 +0500

    fix: added google calendar link in timeline section

--- src/consts/timeline.ts
+++ src/consts/timeline.ts
@@ -9,6 +9,7 @@ export const TimelineData: ITimelineData[] = [
   {
     question: "When?",
     answer: "13-14 April",
+    link: "https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MGFsMDVncTY2aWowZDdtcnJqNnFiZGI3MjMgY19lZmQ0NzM5NTAyNGM1ZTMzN2U2Njc4YWM5MmVlYzVmNjJjYWFjZWNkOWU3ZmMzM2YwNDFjMzI1MTIyNzU5MDMzQGc&tmsrc=c_efd47395024c5e337e6678ac92eec5f62caacecd9e7fc33f041c325122759033%40group.calendar.google.com",
   },
   {
     question: "Where?",
