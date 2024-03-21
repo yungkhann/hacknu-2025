e3fa7a55161ab11be73ea335889b4abbaf07bace
Abyl10 <abylay.kairatbek@gmail.com>
2024-03-21 17:48:18 +0500

    fix: vercel rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/register",
+          destination: "https://hack-nu-2024-registration.vercel.app/",
+        },
+      ],
+    };
+  },
+};
 
 export default nextConfig;
