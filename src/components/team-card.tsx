885e8e39d8a033a75d3d8b08cac350eb10a22b15
Abdrakhman <abdrakhman@Abdrakhmans-MacBook-Pro.local>
2024-03-22 00:37:59 +0500

    add blinking

--- src/components/team-card.tsx
+++ src/components/team-card.tsx
@@ -4,7 +4,7 @@ import { TeamInfo } from "@/app/t/[id]/page";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import Link from "next/link";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const Card = (teamInfo: TeamInfo) => {
   const formatTeamData = useCallback(() => {
@@ -19,36 +19,38 @@ export const Card = (teamInfo: TeamInfo) => {
     const element = document.getElementById("card") as HTMLDivElement | null;
     if(!element) return;
 
-    const width = 297 * 2 - 48;
-    const height = 210 * 2 - 80;
+    const width = 297 * 3 - 360;
+    const height = 210 * 3 - 300;
     const body = document.createElement('div');
     body.style.display = 'flex';
     body.style.flexDirection = 'column';
     body.style.alignItems = 'center';
     body.style.justifyContent = 'center';
     body.style.padding = '24px 40px'
-    body.style.width = `${width + 48}px`;
-    body.style.height = `${height + 80}px`;
+    body.style.width = `${width + 360}px`;
+    body.style.height = `${height + 300}px`;
     body.style.wordSpacing = '4px';
     body.style.backgroundColor = '#191919';
+
     const newElement = element.cloneNode(true) as HTMLDivElement;
 
     newElement.style.width = `${width}px`;
     newElement.style.height = `${height}px`;
 
-    newElement.childNodes[0].childNodes[0].removeChild(newElement.childNodes[0].childNodes[0].childNodes[1]);
+    if(newElement.firstChild?.firstChild?.lastChild)
+      newElement.firstChild?.firstChild?.lastChild.remove()
     body.appendChild(newElement);
 
     body.style.position = 'absolute';
     body.style.left = '-9999px';
     document.body.appendChild(body);
 
-
     const canvas = await html2canvas(body , {
       backgroundColor: "#191919",
       scale: 2,
     });
-    const data = canvas.toDataURL('image/png', 1.0);
+    document.body.removeChild(body);
+    const data = canvas.toDataURL('image/png' );
 
     const pdf = new jsPDF({
       orientation: 'landscape',
@@ -77,6 +79,7 @@ export const Card = (teamInfo: TeamInfo) => {
     pdf.save(`#${teamInfo.team_code}.pdf`);
   }, [teamInfo]);
 
+
   return (
     <div id='card' className={"relative flex md:w-[500px] p-[10px] items-start gap-4 border-dashed border-2 border-dashed-border"}>
       <div className="flex w-full h-full flex-1 p-6 md:p-8 flex-col justify-center items-start gap-12 border-dashed	border-2 border-dashed-border">
@@ -95,8 +98,8 @@ export const Card = (teamInfo: TeamInfo) => {
           <div className="flex flex-1">
             <p className="opacity-50 leading-[140%] font-light whitespace-pre-line">{'13-14 April\nNazarbayev University'}</p>
           </div>
-          <button onClick={generatePdf}>
-            <p className="flex flex-1 leading-[140%] text-primary-green font-bold whitespace-pre-line">{'> Save'}</p>
+          <button onClick={generatePdf} className="flex flex-row gap-2">
+            <p className="min-w-3 leading-[140%] text-primary-green font-bold whitespace-pre-line animate-blink">{'>'}</p><p className="flex flex-1 leading-[140%] text-primary-green font-bold whitespace-pre-line">{'Save'}</p>
           </button>
         </div>
       </div>
