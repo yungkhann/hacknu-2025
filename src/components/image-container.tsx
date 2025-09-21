991e3bf35fe4fc0b04fd821af46599f9685ca870
yungkhann <ertaev.19102007@gmail.com>
2025-09-21 21:57:58 +0500

    changed image container

--- src/components/image-container.tsx
+++ src/components/image-container.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 interface ImageContainerProps {
   index: number;
@@ -9,12 +9,20 @@ interface ImageContainerProps {
 const ImageContainer = ({ index, image, event }: ImageContainerProps) => {
   return (
     <div className="relative bg-[#252525]" style={{ paddingTop: '100%' }}>
-      <Image src={image} alt={`Image ${index + 1}`} fill style={{objectFit: "cover"}} sizes="(max-width: 768px) 100vw, 33vw" quality={100}/>
+      <Image
+        src={image}
+        alt={`Image ${index + 1}`}
+        fill
+        style={{ objectFit: 'cover' }}
+        sizes="(max-width: 768px) 100vw, 33vw"
+      />{' '}
       <div className="w-full absolute bottom-0">
         <div className="relative h-[88px]">
           <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#191919]"></div>
           <div className="absolute inset-0 flex items-center justify-center">
-            <p className="absolute left-2 bottom-2 md:left-4 md:bottom-4 text-white text-[12px] md:text-[16px] text">{event}</p>
+            <p className="absolute left-2 bottom-2 md:left-4 md:bottom-4 text-white text-[12px] md:text-[16px] text">
+              {event}
+            </p>
           </div>
         </div>
       </div>
