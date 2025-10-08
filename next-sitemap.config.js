53af04b3a0547a731d2e8c46d91793c2d7a6466d
Ualikhan Yertayev <ualikhanyertayev@Ualikhans-MacBook-Pro.local>
2025-10-08 14:18:29 +0500

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://hacknu.kz",
+  siteUrl: "https://hacknu25.nuacm.kz",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: "weekly",
-  priority: 0.7,
-  exclude: ["/admin", "/private"],
-  robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-  },
 };
